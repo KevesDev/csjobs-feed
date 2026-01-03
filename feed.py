2de0ce4803c462313b88f8bc61bbfa99e417a8e2
Riane <42573189+KevesDev@users.noreply.github.com>
2026-01-03 03:55:03 -0700

    Refactor feed.py to fetch job data from RSS

--- feed.py
+++ feed.py
@@ -1,32 +1,30 @@
 import yaml
 import xml.etree.ElementTree as xml_tree
+import requests
 
 with open('feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
 
-    rss_element = xml_tree.Element('rss', {'version':'2.0',
-        'xmlns:utnes':'http://www.itunes.com/dtds/podcast-1.0.dtd',
-        'xmlns:content':'http://purl.org/rss/1.0/modules/content/'})
-    
+rss_element = xml_tree.Element('rss', {'version': '2.0'})
 channel_element = xml_tree.SubElement(rss_element, 'channel')
 
-link_prefix = yaml_data['link']
-
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
-xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
-xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
-xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
+xml_tree.SubElement(channel_element, 'link').text = yaml_data['link']
 xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
-xml_tree.SubElement(channel_element, 'itunes:image', {'href': link_prefix + yaml_data['image']})
 xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
-xml_tree.SubElement(channel_element, 'link').text = link_prefix
 
-xml_tree.SubElement(channel_element, 'itunes:category', {'text': yaml_data['category']})
+# Pull CS jobs from RSS feed
+feed = xml_tree.fromstring(
+    requests.get('https://weworkremotely.com/categories/remote-programming-jobs.rss').content
+)
 
-for item in yaml_data['item']:
+for item in feed.findall('./channel/item'):
     item_element = xml_tree.SubElement(channel_element, 'item')
-    xml_tree.SubElement(item_element, 'title').text = item['title']
-    xml_tree.SubElement(item_element, 'description').text = item['description']
+
+    xml_tree.SubElement(item_element, 'title').text = item.find('title').text
+    xml_tree.SubElement(item_element, 'link').text = item.find('link').text
+    xml_tree.SubElement(item_element, 'description').text = item.find('description').text
+    xml_tree.SubElement(item_element, 'pubDate').text = item.find('pubDate').text
 
 output_tree = xml_tree.ElementTree(rss_element)
 output_tree.write('jobsfeed.xml', encoding='UTF-8', xml_declaration=True)
